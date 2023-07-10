8b98c7e9b297d255de2d81578c2b07f210ecac38
dsivanov5372 <mitya5372@yandex.ru>
2023-07-10 15:26:21 +0300

    add exceptions to track in error handler

--- cms/src/main/java/dia/kids/cms/exception/ErrorHandler.java
+++ cms/src/main/java/dia/kids/cms/exception/ErrorHandler.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler
+    @ExceptionHandler(value = { PictureNotFoundException.class,
+                                ArticleNotFoundException.class,
+                                CommentNotFoundException.class })
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final RuntimeException exception) {
         return new ErrorResponse(exception.getMessage());
